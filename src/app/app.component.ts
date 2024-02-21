006d141f5397f8eeba523f7753146b87e87a921d
Alan Huang <alan.huang@crisphealth.org>
2024-02-21 00:51:28 -0500

    change role and add logout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  template: `<a href="/.auth/login/aad">Login</a><div>{{message}}</div>`,
+  template: `<a href="/.auth/login/aad">Login</a><div>{{message}}</div><a href="/.auth/login/aad">Logouot</a>`,
 })
 export class AppComponent {
   message = '';
